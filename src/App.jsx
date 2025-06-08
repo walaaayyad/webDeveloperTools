ac66d6c651db535ad05d9ccb146c08e220b7b45f
Walaa Ayyad <walaa_ayyad7@yahoo.com>
2025-06-08 21:39:32 +0300

    add react-helmet

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,12 @@ import Hero from "./components/2-heroSection";
 import ButtonsBox from "./components/3-buttonsBox";
 import ToolsBox from "./components/5-toolsBox";
 import Footer from "./components/6-footer";
-// import OffCanvas from "./components/4-offCanvas";
 import { ButtonProvider } from "./ButtonContext"; // Import the provider
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import './i18n';
+import { Helmet } from "react-helmet";
+
 
 
 function App() {
@@ -33,19 +34,26 @@ useEffect(() => {
 
   return (
     <ButtonProvider>
-        <Container fluid>
-          <div className="bg-circle bg-left-circle"></div>
-          <div className="bg-circle bg-right-circle"></div>
-          <NavBar />
-          <Hero />
-          <ButtonsBox onClick={scrollToTarget}/>
-          <ToolsBox targetRef={targetRef}/>
-          <Footer/>    
-        </Container>
+      <Helmet>
+        <title>Free Tools for Web Developers | Inspiration, Design, Assets, CSS tools & More</title>
+        <meta name="description" content="Discover a curated collection of free tools and resources for web developers — including inspirational websites, assets, CSS tools, and optimization tools — all in one place." />
+        <meta name="keywords" content="web development tools, free developer tools, icons, fonts, inspiration, design resources, web design" />
+        <meta name="author" content="Walaa Ayyad" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Helmet>
+      <Container fluid>
+        <div className="bg-circle bg-left-circle"></div>
+        <div className="bg-circle bg-right-circle"></div>
+        <NavBar />
+        <Hero />
+        <ButtonsBox onClick={scrollToTarget}/>
+        <ToolsBox targetRef={targetRef}/>
+        <Footer/>    
+      </Container>
     </ButtonProvider>
   );
 }
 
 export default App;
 
-//design-----[https://dribbble.com/shots/20184234-Metaverse-Education-Website-UI-UX-Design]
+
